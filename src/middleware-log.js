349af51f5fd364e1519056911fc1a6f2b1c132ca
filipi.fuchter <filipi.fuchter@digitro.com.br>
2018-10-30 09:14:28 -0300

    Utilizando o middleware de log

--- src/middleware-log.js
+++ src/middleware-log.js
@@ -1,7 +1,9 @@
 const JsonLog = require('./json-log')
 
-const logRequest = (service, logLevel) => {
-  const logger = new JsonLog('queue-service', logLevel || 'INFO')
+const logRequest = (service) => {
+  //TODO recuperar o log level da API
+  const logLevel = 'INFO'
+  const logger = new JsonLog('queue-service', logLevel)
   return function(req, res, next) {
     //identificador da requisição
     const valId = req.headers['X-VAL-ID'] || Math.random();

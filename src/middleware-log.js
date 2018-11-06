f6a7cb8b274322957e7728feac0b4a521b32a60c
filipi.fuchter <filipi.fuchter@digitro.com.br>
2018-11-06 09:28:27 -0200

    Permitindo pegar o valId tambem como query param

--- src/middleware-log.js
+++ src/middleware-log.js
@@ -12,7 +12,7 @@ const logRequest = (app, service, generateValId) => {
 
   return function(req, res, next) {
     //identificador da requisição
-    let valId = req.header(LOG_CONSTANTS.TRACE_HEADER)
+    let valId = req.header(LOG_CONSTANTS.TRACE_HEADER) || req.query.valId
     if(!valId && generateValId){
       valId = uuidv1()
       req.headers[LOG_CONSTANTS.TRACE_HEADER] = valId
@@ -21,8 +21,9 @@ const logRequest = (app, service, generateValId) => {
       res.set(LOG_CONSTANTS.TRACE_HEADER, valId)
     }
     //logando as informações
-    const tags = {url: req.url, params: req.params, method:req.method, valId:valId}
-    logger.debug('rest request => ' + valId, tags)
+    const srcIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+    const tags = {srcIp:srcIp, url: req.url, params: req.params, query: req.query, method:req.method, valId:valId}
+    logger.debug('[REST request log interceptor]', tags)
     next()
   }
 }

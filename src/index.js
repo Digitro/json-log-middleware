b87176b1d3dac27dba428a3cfe0ee1d530a48d54
filipi.fuchter <filipi.fuchter@digitro.com.br>
2018-11-07 08:50:48 -0200

    Padronizando nomenclatura

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const JsonLog = require('./json-log')
-const LogMiddlewareRest = require('./middleware-log')
+const logMiddleware = require('./middleware-log')
 const LOG_CONSTANTS = require('./log-constants')
 const logContext = require('./log-context')
 
-module.exports = {JsonLog, LogMiddlewareRest, LOG_CONSTANTS, logContext}
+module.exports = {JsonLog, logMiddleware, LOG_CONSTANTS, logContext}
